e740a63b37a3bf0ee15e531fef436349e663a93b
Jaakko Leskinen <jaakko.leskinen@gmail.com>
2025-04-29 11:43:30 +0000

    feat: Initial oh-my-taskfiles implementation
    This commit introduces the initial implementation of oh-my-taskfiles, a taskfile (go-task) based library to help with daily tasks.
    
    The changes include:
    
    -   `install.sh`: A script to install oh-my-taskfiles, clone the repository, install go-task, and provide instructions for bash integration.
    -   `README.md`: A basic README file with installation instructions.
    -   `taskfile.yaml`: A taskfile that includes `taskfile.omt.yaml` and defines a default task to list all available tasks.
    -   `taskfile.omt.yaml`: A taskfile containing oh-my-taskfiles specific tasks, currently with an "update" task.

---
+++ README.md
@@ -0,0 +1,9 @@
+# Oh-My-Taskfiles!
+
+A taskfile (go-task) based library to help with the daily grind.
+
+## Installation
+
+```bash
+sh -c "$(curl --location https://raw.githubusercontent.com/NeonTowel/oh-my-taskfiles/refs/heads/main/install.sh)"
+```
---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+echo "Installing Oh-My-Taskfiles..."
+
+if [ ! -d "$HOME/.omt" ]; then
+  echo "Cloning to $HOME/.omt..."
+  git clone https://github.com/NeonTowel/oh-my-taskfiles.git "$HOME/.omt"
+
+  echo "Installing go-task to $HOME/bin..."
+  sh -c "$(curl --location https://taskfile.dev/install.sh)" -- -d -b $HOME/bin
+
+  # Detect if running in bash and print instructions for the user
+  if [ -n "$BASH_VERSION" ]; then
+    echo
+    echo "To complete the setup, please add the following to your ~/.bashrc file:"
+    echo
+    echo "  export PATH=\$HOME/.omt/bin:\$PATH"
+    echo "  alias omt='task -d $HOME/.omt'"
+    echo
+    echo "Then, reload your bash configuration by running:"
+    echo "  source ~/.bashrc"
+  fi
+
+else
+  echo "Directory $HOME/.omt already exists, aborting..."
+fi
